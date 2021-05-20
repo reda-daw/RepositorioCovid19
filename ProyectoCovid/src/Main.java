adaef5afbb3db1f9bae62d1eea0ed922595faeeb
mint <mint@mint-VirtualBox>
2021-05-20 09:37:19 +0200

    Segunda Verison.

--- ProyectoCovid/src/Main.java
+++ ProyectoCovid/src/Main.java
@@ -10,6 +10,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esta es una aplicación para gestionar los pacientes Covid");
+		System.out.println("Probando 1...");
+		System.out.println("Probando 2...");
 
 	}
 
