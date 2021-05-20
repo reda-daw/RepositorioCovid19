7bb64a7114c01a21db6d47cf0999536226ef0ea4
mint <mint@mint-VirtualBox>
2021-05-20 09:20:25 +0200

    Primera Version del proyecto solo contiene un saludo.

---
+++ ProyectoCovid/src/Main.java
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * @author Reda Merimi Jabri
+ *@version 1.0.0
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esta es una aplicación para gestionar los pacientes Covid");
+
+	}
+
+}
